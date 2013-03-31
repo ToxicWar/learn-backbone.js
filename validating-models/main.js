20df658482d3821bfb7d6fe46f41ace0cef9b9e7
ToxicWar <toxicwar94@yandex.ru>
2013-04-01 00:01:36 +0400

    Add 'Validating Models'

---
+++ validating-models/main.js
@@ -0,0 +1,22 @@
+﻿var Person = Backbone.Model.extend({
+    defaults: {
+        first_name: 'Anton',
+        last_name: 'Larkin',
+        age: 19,
+        occupation: 'python/django developer'
+    },
+
+    validate: function(attrs) {
+        if (attrs.age < 0) {
+            return 'Age must be positive.';
+        }
+
+        if (!attrs.first_name || !attrs.last_name) {
+            return 'Every person must have a first name and last name.';
+        }
+    },
+
+    work: function() {
+        return this.get('first_name') + ' ' + this.get('last_name') + ' is working.';
+    }
+});
