c452f7fe0807f7c1613977c3ddc03699b36326bf
ToxicWar <toxicwar94@yandex.ru>
2013-03-31 21:58:05 +0400

    Add 'Data Types'

---
+++ data-types/main.js
@@ -0,0 +1,9 @@
+﻿var Person = function(config) {
+    this.first_name = config.first_name;
+    this.age = config.age;
+    this.hobby = config.hobby;
+};
+
+Person.prototype.work = function() {
+    return this.first_name + ' is working.';
+};
