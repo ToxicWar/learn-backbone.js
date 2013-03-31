9fc3d964b13008544206f446c0b66fcbdaf82a74
ToxicWar <toxicwar94@yandex.ru>
2013-04-01 00:02:12 +0400

    Add 'Views'

---
+++ views/main.js
@@ -0,0 +1,25 @@
+﻿var Person = Backbone.Model.extend({
+    defaults: {
+        first_name: 'Anton',
+        last_name: 'Larkin',
+        age: 19,
+        occupation: 'python/django developer'
+    }
+});
+
+var PersonView = Backbone.View.extend({
+    tagName: 'li',
+
+    initialize: function() {
+        this.render();
+    },
+
+    render: function() {
+        // anti-pattern
+        this.$el.html(this.model.get('first_name') + ' ' + this.model.get('last_name') +
+            ' (' + this.model.get('age') + ') - ' + this.model.get('occupation'));
+    }
+});
+
+var person = new Person();
+var personView = new PersonView({ model: person });
