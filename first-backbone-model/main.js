8e34fb2184312f0e0628ed6a8299b1c2375ebca6
ToxicWar <toxicwar94@yandex.ru>
2013-03-31 21:59:18 +0400

    Add 'First Backbone Model'

---
+++ first-backbone-model/main.js
@@ -0,0 +1,12 @@
+﻿var Person = Backbone.Model.extend({
+    defaults: {
+        first_name: 'Anton',
+        last_name: 'Larkin',
+        age: 19,
+        occupation: 'python/django developer'
+    },
+
+    work: function() {
+        return this.get('first_name') + ' ' + this.get('last_name') + ' is working.';
+    }
+});
