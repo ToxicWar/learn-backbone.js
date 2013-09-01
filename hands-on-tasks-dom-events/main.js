328dd6e5a48bae9ece41bf63474e48569fe4f2f8
ToxicWar <toxicwar94@yandex.ru>
2013-09-01 14:28:16 +0400

    Add 'Hands On Tasks Dom Events'

---
+++ hands-on-tasks-dom-events/main.js
@@ -0,0 +1,72 @@
+(function() {
+
+    window.App = {
+        Models: {},
+        Collections: {},
+        Views: {}
+    };
+
+    window.template = function(id) {
+    return _.template( $('#' + id).html() );
+    };
+
+    App.Models.Task = Backbone.Model.extend({});
+
+    App.Collections.Tasks = Backbone.Collection.extend({
+        model: App.Models.Task
+    });
+
+    App.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
+
+        render: function() {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+
+        addOne: function(task) {
+            var taskView = new App.Views.Task({model:task});
+            this.$el.append(taskView.render().el);
+        }
+    });
+
+    App.Views.Task = Backbone.View.extend({
+        tagName: 'li',
+
+        template: template('taskTemplate'),
+
+        events: {
+            'click .edit': 'editTask'
+        },
+
+        editTask: function() {
+            var newTaskTitle = prompt('What would you like change the text to?', this.model.get('title'));
+            this.model.set('title', newTaskTitle);
+        },
+
+        render: function() {
+            var template = this.template(this.model.toJSON());
+            this.$el.html(template);
+            return this;
+        }
+    });
+
+    window.tasksCollection = new App.Collections.Tasks([
+        {
+            title: 'Go to the store',
+            priority: 4
+        },
+        {
+            title: 'Go to the mail',
+            priority: 3
+        },
+        {
+            title: 'Get to work',
+            priority: 5
+        }
+    ]);
+
+    var taskView = new App.Views.Tasks({collection: tasksCollection})
+    $(".tasks").html(taskView.render().el);
+
+})();
