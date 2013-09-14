dc76db59357bc47372e9e86bc7e15694c2775162
ToxicWar <toxicwar94@yandex.ru>
2013-09-15 01:45:07 +0400

    Add "Fetching-Rows-Into-A-Collection"

---
+++ fetching-rows-into-a-collection/main.js
@@ -0,0 +1,60 @@
+﻿(function() {
+
+	window.App = {
+		Models: {},
+		Collections: {},
+		Views: {}
+	};
+
+	App.Models.Task = Backbone.Model.extend({
+		defaults: {
+			title: '',
+			is_completed: false
+		},
+		urlRoot: 'tasks/'
+	});
+
+    App.Collections.Tasks = Backbone.Collection.extend({
+        model: App.Models.Task,
+        url: 'tasks/'
+    });
+
+    App.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
+
+        initialize: function() {
+            this.collection.on('add', this.addOne, this);
+        },
+
+        render: function() {
+            this.$el.empty();
+            this.collection.each(this.addOne, this)
+            return this;
+        },
+
+        addOne: function(task) {
+            var task = new App.Views.Task({model: task});
+            this.$el.append(task.render().el);
+        }
+    });
+
+    App.Views.Task = Backbone.View.extend({
+        tagName: 'li',
+
+        initialize: function() {
+            this.model.on('destroy', this.remove, this);
+        },
+
+        render: function() {
+           this.$el.html(this.model.get('title'));
+           return this;
+        }
+    });
+
+    window.tasksCollection = new App.Collections.Tasks;
+    window.tasksCollection.fetch();
+
+    var tasksView = new App.Views.Tasks({collection: tasksCollection})
+	$(".tasks").html(tasksView.render().el);
+
+})();
